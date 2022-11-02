8fa6fbf9b7ce8288e500e274bb1c8c5d5f8eac67
Churina Sa <113448247+Churina@users.noreply.github.com>
2022-11-02 14:55:35 -0400

    to do list

--- assignment/js-dom-lecture-activity/index.js
+++ assignment/js-dom-lecture-activity/index.js
@@ -1,13 +1,43 @@
+
 const listItems = document.querySelectorAll('li');
 const addCompleted = (event) =>
 {
-    Event.target.classlist.toggle('completed');
+    event.target.classList.toggle('completed');
+}
+for(const thisItem of listItems){
+    thisItem.addEventListener('click', addCompleted)
 }
 
-for(const thisItem of listItems)
+/*
+const onAddToDo = (event) => 
 {
-    thisItem.addEventListener('click',(event) => 
-    {
-        event.target.classList.toggle('completed');
-    })
+    const newListItem = document.createElement('li')
+    const text = document.createTextNode('figure this part out later');
+    newListItem.appendChild(text);
+    document.querySelector('ul').appendChild(newListItem);
 }
+document.querySelector('button').addEventListener('click', onAddToDo)
+*/
+
+
+/*
+const onAddToDo = (event) => {
+    const text = event.target.parentNode.querySelector('input').value;
+    const newListItem = document.createElement('li');
+    const liText= document.createTextNode(text);
+    newListItem.appendChild(liText);
+    document.querySelector('ul').appendChild(newListItem);
+}
+document.querySelector('button').addEventListener('click', onAddToDo)
+*/
+
+
+const onAddToDo = (event) => {
+    const text = event.target.parentNode.querySelector('input').value;
+    const newListItem = document.createElement('li');
+    newListItem.addEventListener('click', addCompleted);
+    const liText= document.createTextNode(text);
+    newListItem.appendChild(liText);
+    document.querySelector('ul').appendChild(newListItem);
+    }
+    document.querySelector('button').addEventListener('click', onAddToDo)
